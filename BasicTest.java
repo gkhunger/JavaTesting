919e9ff657ebb6101f291c19e24d9d992859711f
gkhunger <gaurav.khunger@safenet-inc.com>
2015-09-28 15:39:09 -0400

    Project setup

--- BasicTest.java
+++ BasicTest.java
@@ -4,4 +4,8 @@ public class BasicTest extends TestCase {
   public void testTrue() {
     assertTrue(true);
   }
+ 
+  public void testSum(){
+    Travis travis = new Travis();
+    assertTrue(travis.sum(1,2).equals(3));
 }
