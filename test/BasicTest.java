629dd4373c09b7bce8fd2b4bf17e08e38f9e45c3
gkhunger <gaurav.khunger@safenet-inc.com>
2015-09-28 16:43:23 -0400

    version checks

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -2,12 +2,16 @@ package travis;
 import junit.framework.TestCase;
 
 public class BasicTest extends TestCase {
+  Travis travis = new Travis();
   public void testTrue() {
     assertTrue(true);
   }
  
   public void testSum(){
-    Travis travis = new Travis();
     assertTrue(travis.sum(1,2) == 3);
+  }
+
+  public void testVersion(){
+    assertTrue(travis.getTypeOfDayWithSwitchStatement("Sunday").equals("Weekend"));
   }
 }
