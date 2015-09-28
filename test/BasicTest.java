60fb329300a512b1ca1eb5a8adf055d74eb83391
gkhunger <gaurav.khunger@safenet-inc.com>
2015-09-28 16:30:40 -0400

    test case fixed

---
+++ test/BasicTest.java
@@ -0,0 +1,13 @@
+package travis;
+import junit.framework.TestCase;
+
+public class BasicTest extends TestCase {
+  public void testTrue() {
+    assertTrue(true);
+  }
+ 
+  public void testSum(){
+    Travis travis = new Travis();
+    assertTrue(travis.sum(1,2) == 3);
+  }
+}
---
+++ test/Travis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travis;
+
+/**
+ *
+ * @author gkhunger
+ */
+public class Travis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public int sum(int a, int b){
+        return a+b;
+    }
+    
+}
